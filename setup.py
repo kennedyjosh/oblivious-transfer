df3ce8fd26b2ceefb961f9aec6c9bea28b280877
Josh <kennedyjosh99@gmail.com>
2024-05-24 01:55:57 -0500

    More updates to setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from distutils.core import setup
 
 setup(
     name='oblivious-transfer',
-    version='',
+    version='1.0',
     packages=['ot'],
     url='https://github.com/kennedyjosh/oblivious-transfer',
-    license='',
+    license='MIT',
     author='josh',
     author_email='',
     description='',
